7bd88ce334fb6e876d1fa6bec74a1e446d551212
Varun <varun140801@gmail.com>
2023-09-06 02:40:19 +0530

    update on slider

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,6 +1,19 @@
 "use client";
 
-const Slider = () => {
+import * as RasixSlider from "@radix-ui/react-slider";
+
+interface SliderProps {
+    value?: number;
+    onChange?: (value: number) => void;
+}
+
+const Slider: React.FC<SliderProps> = ({
+    value = 1,
+    onChange
+}) => {
+    const handleChange = (newValue: number[]) => {
+        onChange?.(newValue[0]);
+    }
     return ( 
         <div>
             slider
